414fb87bd0dec3867443195480f90ebae0050dce
Carl Schéle <carl.schele@esatto.se>
2026-09-17 10:21:10 +0200

    Add media model updates

---
+++ Models/Media/ImageFile.cs
@@ -0,0 +1,10 @@
+﻿using EPiServer.Framework.DataAnnotations;
+
+namespace Optimizely26.Models.Media
+{
+    [ContentType(GUID = "B3FE2ADD-56E6-4018-AD98-65F9330CFB3B")]
+    [MediaDescriptor(ExtensionString = "jpg,jpeg,png,gif")]
+    public class ImageFile : ImageData
+    {
+    }
+}
